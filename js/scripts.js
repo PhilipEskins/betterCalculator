be2ae3465f3f979fd1579dc5106c8572a8cf7b27
Joe Pritchett and Philip Eskins <philip@>
2019-01-16 13:49:15 -0800

    add logic to get radio buttons work

--- js/scripts.js
+++ js/scripts.js
@@ -20,48 +20,22 @@ function div(n1, n2) {
 
 $(document).ready(function() {
 
-  $("form#add").submit(function(event) {
+  $("form#calculate").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#addResult").text(result);
-  });
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+    var operationInput = $("input:radio[name=operation]:checked").val();
 
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = sub(number1, number2);
-    $("#subtractResult").text(result);
+    if (operationInput==="add") {
+      document.write("Your total is " + add(number1, number2));
+    } else if (operationInput==="sub") {
+      document.write("Your total is " + sub(number1, number2));
+    } else if (operationInput==="mult") {
+      document.write("Your total is " + mult(number1, number2));
+    } else if (operationInput==="div") {
+      document.write("Your total is " + div(number1, number2));
+    } else {
+      document.write("Not a valid function");
+    }
   });
-
-  $("form#mult").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mult1").val());
-    var number2 = parseInt($("#mult2").val());
-    var result = mult(number1, number2);
-    $("#multResult").text(result);
-  });
-
-  $("form#divi").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#divi1").val());
-    var number2 = parseInt($("#divi2").val());
-    var result = div(number1, number2);
-    $("#diviResult").text(result);
-  });
-
 });
-
-/*if (operation==="add") {
-  document.write("Your total is " + add(number1, number2));
-} else if (operation==="sub") {
-  document.write("Your total is " + sub(number1, number2));
-} else if (operation==="mult") {
-  document.write("Your total is " + mult(number1, number2));
-} else if (operation==="div") {
-  document.write("Your total is " + div(number1, number2));
-} else {
-  document.write("Not a valid function");
-}*/
