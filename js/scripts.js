4a8d94e5a61acd728dbde2ed695530de96347bc3
Joe Pritchett and Philip Eskins <philip@>
2019-01-16 10:36:16 -0800

    style to add additional functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-var number1 = parseInt(prompt("What is the first number?"));
+/*var number1 = parseInt(prompt("What is the first number?"));
 var number2 = parseInt(prompt("What is the second number?"));
-var operation = prompt("What would you like to do?")
+var operation = prompt("What would you like to do?")*/
 
 function add(n1, n2) {
   return n1 + n2;
@@ -13,11 +13,22 @@ function sub(n1, n2) {
 function mult(n1, n2) {
   return n1 * n2;
 }
+
 function div(n1, n2) {
   return n1 / n2;
 }
 
-if (operation==="add") {
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $(".output").text(result);
+  });
+});
+
+/*if (operation==="add") {
   document.write("Your total is " + add(number1, number2));
 } else if (operation==="sub") {
   document.write("Your total is " + sub(number1, number2));
@@ -27,4 +38,4 @@ if (operation==="add") {
   document.write("Your total is " + div(number1, number2));
 } else {
   document.write("Not a valid function");
-}
+}*/
